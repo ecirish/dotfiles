d50eee856cea5ab62b0e78e40319b929443819f5
Eric Irish <ecirish@gmail.com>
2020-06-30 15:31:30 -0400

    Revert "switch fish-nvm package"
    This reverts commit d0087700d84885e374d4defcd96bd6287c11292b.

--- apt-init/installers/dev.sh
+++ apt-init/installers/dev.sh
@@ -30,7 +30,8 @@ sudo -u $USER \
   curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher && \
 
 log "Installing fish plugins:" && \
-sudo -u $USER fish -c "fisher add jorgebucaran/fish-nvm" && \
+sudo -u $USER fish -c "fisher add FabioAntunes/fish-nvm" && \
+sudo -u $USER fish -c "fisher add edc/bass" && \
 
 log "Installing Node.js via nvm:" && \
 fish -c 'nvm install 13.11.0' && \
